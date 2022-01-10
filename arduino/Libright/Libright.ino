e787ed0fc9ded773529c0df6d26dd415cb0c6438
Gros19 <kjj6393@gmail.com>
2022-01-10 17:00:34 +0900

    버그 수정

--- arduino/Libright/Libright.ino
+++ arduino/Libright/Libright.ino
@@ -2,8 +2,10 @@
 /*
  * 시간에 관한 변수
  */
-int what_minute = 0;
-int ONE_MINUTE = 10000;
+int what_time = 0;
+int minute = 0;
+int TIME = 1000;
+
 
 /*
  * 수면 조건에 관한 변수
@@ -14,7 +16,7 @@ boolean before_sleep_mode = false;
 /*
  * 적외선 거리 센서에 관한 변수
  */
-int rlsen = 8;
+int rlsen = 13;
 /*
  * 적외선 거리 센서의 측정값
  */
@@ -23,64 +25,101 @@ int senval = 0;
 /*
  * 조건 테스트용 pin
  */
-int testPin = 2;
- 
+int testPin = 12;
+
 /*
  * led pin에 관한 변수
  */
-int ledPin[4] = {5, 6, 9, 10};
-
+int ledPinA[4] = {10, 5, 6, 9};
+int ledPinB[4] = {2, 4, 7, 8};
 /*
  * led 밝기에 관한 변수
  */
-int bright = 0;
+
 int bright255 = 0;
-int dimmer = 0;
+int dimmer = 1;
+
+void LedAON();
+void LedAOFF();
+void LedBON();
+void LedBOFF();
 
 void oneMinuteCount(){
-  what_minute += 1;
-  Serial.print(what_minute);
-  Serial.println("분입니다.");
+ ++what_time;
+ /*
+  * 60초가 되면
+  */
+ if(what_time > 59){
   /*
-   * 15분이 지나면 what_minute를 0으로 초기화
-   * 0 ~ 15분 반복
+   * 시간을 0초로 초기화
    */
-  if(what_minute > 14*6){ 
-    what_minute = 0;
+  oneFiveMinuteProc();
+  what_time = 0;
+
+   /*
+   * 만약 before_sleep_mode 모드이면
+   * 밝기를 조금씩 어둡게 한다.
+   */
+  if(before_sleep_mode == true){
+    dimmer += 17;
+  }else{
+    bright255 = analogRead(0)>>2;
+  }
+ }
+}
+
+void oneFiveMinuteProc(){
+  ++minute;
+  /*
+   * 15분이되면
+   */
+  if(minute > 14){
     /*
-     * 15분간 non_sleep_proof < 3 보다 작아 책장을 거의 넘기지 않았다명
-     * before_sleep_mode로 돌입하고
+     * 0분으로 초기화
+     */
+    minute = 0;
+    /*
+     * 15분동안 책을 안 읽었으면
+     * 슬립모드로 전환
      */
     if(non_sleep_proof < 5){
       before_sleep_mode = true;
-      Serial.println("수면모드 돌입");
-    }
-    else{
+    }else{
+      before_sleep_mode =false;
       non_sleep_proof = 0;
-      Serial.println("책 잘 읽는 중");
     }
+
   }
 }
 void setup() {
  Serial.begin(9500);
  //testPin ouput setting
- pinMode(testPin, OUTPUT); 
+ pinMode(testPin, OUTPUT);
 
  //sensor pin input setting
  pinMode(rlsen, INPUT);
- 
+
  //led pin output setting
- pinMode(ledPin[0], OUTPUT);
- pinMode(ledPin[1], OUTPUT);
- pinMode(ledPin[2], OUTPUT);
- pinMode(ledPin[3], OUTPUT);
+ pinMode(ledPinA[0], OUTPUT);
+ pinMode(ledPinA[1], OUTPUT);
+ pinMode(ledPinA[2], OUTPUT);
+ pinMode(ledPinA[3], OUTPUT);
+ pinMode(ledPinB[0], OUTPUT);
+ pinMode(ledPinB[1], OUTPUT);
+ pinMode(ledPinB[2], OUTPUT);
+ pinMode(ledPinB[3], OUTPUT);
 
  //MsTimer2 func setting
- //ONE_MINUTE * 15 time over then count func call
- MsTimer2::set(ONE_MINUTE, oneMinuteCount);
+ //TIME * 15 time over then count func call
+ MsTimer2::set(TIME, oneMinuteCount);
 
  //MsTimer2 func start
  MsTimer2::start();
+ Serial.println("strart!");
+ bright255 = 255;
+ LedBON();
+ LedAON();
+
 }
 
 void loop() {
@@ -89,88 +128,100 @@ void loop() {
    * 적외선 거리 센서의 값을 변수로 선언
    * 값을 읽어 저장
    */
-  senval = digitalRead(rlsen); 
- 
+  senval = digitalRead(rlsen);
+
 
     /*
-     * senval이 HIGH면 
+     * senval이 HIGH면
      * 책을 읽고 있고, 잠을 자지 않는다고 판단
      * non_sleep_proof를 1증가 시킨다.
      */
     if(senval == HIGH){
       digitalWrite(testPin, HIGH);
       non_sleep_proof ++;
+      delay(1000);
       /*
-       * 만약 before_sleep_mode인 상태에서 
+       * 만약 before_sleep_mode인 상태에서
        * non_sleep_proof가 2보다 크다면
        * before_sleep_mode에서 탈출하고
        * 밝기를 조금 올린다.
        */
-      if(non_sleep_proof > 5 and before_sleep_mode == true){
+      if(before_sleep_mode == true and non_sleep_proof > 5){
         before_sleep_mode = false;
         Serial.println("sleep모드 탈출!");
+        non_sleep_proof = 0;
         dimmer = 0;
+        LedBON();
       }
     }
     else{
       digitalWrite(testPin, LOW);
     }
 
-   
-    
-
-  /*
-   * 밝기를 수동 조절하기 위해 rotaition senseo 사용
-   * A0번 핀에서 아날로그 입력을 받고 변수 bright에 저장
-   */
-  bright = analogRead(0);
-
-  /*
-  입력 전압 5v일 때 0~255까지 범위를 설정하기 위해
-  int bright255 = bright >> 2;
-  하지만
-  3.3V일 때는 0~172 범위가 축소 됌
-  
-  그래서
-  bright >> 1 하면 0~344 이고
-  */
-  bright = bright >> 1;
-
-  /* 
-   * 그래서 int bright255 bright - bright * 0.3
-   * 344 - (344*0.3) 하면 0 ~ 245 범위를 가짐
-   */
-  bright255 = bright - (bright *(0.3));
-
-   /*
-   * 만약 before_sleep_mode 모드이면
-   * 밝기를  조금씩 어둡게 한다.
-   */
-  if(before_sleep_mode == true){
-    dimmer += 25;
-    
-  }
-  bright255 = bright255 - dimmer;
- 
 
 
-  
-  if(bright255 > 0){
-  analogWrite(ledPin[0], bright255);
-  analogWrite(ledPin[1], bright255);
-  analogWrite(ledPin[2], bright255);
-  analogWrite(ledPin[3], bright255);  
-  }
-  else if (before_sleep_mode == true){
-    Serial.println("수면모드이고 밝기가 0 이하니까 꺼");
-    analogWrite(ledPin[0], LOW);
-    analogWrite(ledPin[1], LOW);
-    analogWrite(ledPin[2], LOW);
-    analogWrite(ledPin[3], LOW);
-    if (bright255 < -100){
-      Serial.println("이제 진짜 종료");
+    if(bright255 < 11){
+      bright255 = 0;
     }
+
+
+
+  if(before_sleep_mode != true){
+//    Serial.println("슬립모드가 아니다");
+    LedAON();
+    LedBON();
+  }
+  else if (bright255 > 0){
+    LedAON();
+    LedBOFF();
+    Serial.print(bright255);
+    Serial.print(" - ");
+    Serial.print(dimmer);
+    Serial.print(" = ");
+    bright255 = bright255 - dimmer;
+    Serial.print(bright255);
+    Serial.println("슬립모드이고 밝기가 양수");
+
+  }else{
+    Serial.println("슬립모드이고 밝기가 0이하");
+     before_sleep_mode = false;
+    LedAOFF();
+    LedBOFF();
+    dimmer = 0;
+    delay(8*360000);
+    /*
+     *   8시간 후 다시 켜기
+     */
   }
   delay(500);
-
 }
+
+
+void LedAON(){
+    analogWrite(ledPinA[0], bright255);
+    analogWrite(ledPinA[1], bright255);
+    analogWrite(ledPinA[2], bright255);
+    analogWrite(ledPinA[3], bright255);
+}
+
+void LedAOFF(){
+    analogWrite(ledPinA[0], LOW);
+    analogWrite(ledPinA[1], LOW);
+    analogWrite(ledPinA[2], LOW);
+    analogWrite(ledPinA[3], LOW);
+}
+
+void LedBON(){
+    digitalWrite(ledPinB[0], HIGH);
+    digitalWrite(ledPinB[1], HIGH);
+    digitalWrite(ledPinB[2], HIGH);
+    digitalWrite(ledPinB[3], HIGH);
+}
+void LedBOFF(){
+    digitalWrite(ledPinB[0], LOW);
+    digitalWrite(ledPinB[1], LOW);
+    digitalWrite(ledPinB[2], LOW);
+    digitalWrite(ledPinB[3], LOW);
+}
+
+
